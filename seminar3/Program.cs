e16ca8e414d7f3714376026ffb02080122d73316
AlexeyMonkey13 <Aptuct89@inbox.ru>
2022-12-24 17:09:52 +0700

    first task

---
+++ seminar3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+
+// ЗАДАЧА СПИСАНА ИЗ ИНТЕРНЕТА ПОЧТИ ПОЛНОСТЬЮ, т.к. сломал голову как развернуть строку или число!!!
+Console.Clear();
+Console.WriteLine("введите число, а мы определим палиндром оно или нет ");
+
+int n = int.Parse(Console.ReadLine());
+int temp = n;
+int Reverse = 0;
+
+while (Convert.ToBoolean(n))
+{
+     int b = n % 10;   
+     Reverse = (Reverse * 10) + b;
+     n = n / 10; 
+}
+Console.WriteLine("реверс   {0}",Reverse);
+
+if (temp == Reverse)
+{
+    Console.WriteLine("да это же ПАЛИНДРОМ");
+}
+else
+{
+    Console.WriteLine("просто число");
+}
+
