9b481c4501940825d198f5613232bc0aaf3d7f35
AlexeyMonkey13 <Aptuct89@inbox.ru>
2022-12-25 13:28:07 +0700

    solved third task

---
+++ task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+
+Console.WriteLine("введите цифру, вообще любую положитльную цифру");
+
+double number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("ваш столбец квадратов");
+
+for (int i = 1; i <= number; i++)
+{
+    Console.WriteLine(i * i * i );
+}
