9570ff1895cb6467b89b2f327e5ed259832c9739
AlexeyMonkey13 <Aptuct89@inbox.ru>
2022-12-25 12:08:53 +0700

    solved the second task

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите три значения для каждой координаты ");
+
+double x1 = Convert.ToInt32(Console.ReadLine());
+double y1 = Convert.ToInt32(Console.ReadLine());
+double z1 = Convert.ToInt32(Console.ReadLine());
+double x2 = Convert.ToInt32(Console.ReadLine());
+double y2 = Convert.ToInt32(Console.ReadLine());
+double z2 = Convert.ToInt32(Console.ReadLine());
+double d = Math.Sqrt(Math.Pow(x1 - (x2), 2) + Math.Pow(y1 - (y2), 2) + Math.Pow(z1 - (z2), 2));
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+Console.WriteLine("ответ: {0}" , d ) ;
